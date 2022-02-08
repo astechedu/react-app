b639c4bde5e3bf9b3e2f577a7dad9012d7107be3
astechutube <astech.utube@gmail.com>
2022-02-08 21:15:27 +0530

    react app

---
+++ src/About.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+   constructor(){
+       super();
+       this.state ={users: []};
+   }
+   componentDidMount() {
+         }
+   render() {
+        return (
+            <div>
+                <h1>About Component</h1>
+                
+            </div>
+        );
+    }
+}
+export default About;
